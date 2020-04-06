cd48401d61c90becc5f54463c4e19d48d146cf81
Marius Vintila <43793363+Mr-Vinti@users.noreply.github.com>
2020-04-06 20:23:17 +0300

    commited initial backend structure

---
+++ backend/src/main/java/com/ag/test/security/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.ag.test.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.microsoft.azure.spring.autoconfigure.aad.AADAuthenticationFilter;
+
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AADAuthenticationFilter aadAuthFilter;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/api/**").authenticated();
+
+		http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/")
+				.deleteCookies("JSESSIONID").invalidateHttpSession(true);
+
+		http.authorizeRequests().anyRequest().permitAll();
+
+		http.csrf().disable();
+		
+		http.cors();
+
+		http.addFilterBefore(aadAuthFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+}
