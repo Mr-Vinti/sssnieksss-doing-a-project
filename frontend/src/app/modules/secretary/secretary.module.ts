e63fcbb06fa8966b9db50606a5ee608225cfe767
andreeaciudin <andreea.ciudin@stud.acs.upb.ro>
2020-05-13 23:42:46 +0300

    Created Secretary, Contract, Certificate modules
    #9
    done 1, 2, 3

--- frontend/src/app/modules/secretary/secretary.module.ts
+++ frontend/src/app/modules/secretary/secretary.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecretaryComponent } from './secretary.component';
+import { ContractComponent } from './contract/contract.component';
+import { CertificateComponent } from './certificate/certificate.component';
+import { SecretaryRouting } from './secretary.routing';
 
 
 
 @NgModule({
-  declarations: [SecretaryComponent],
+  declarations: [SecretaryComponent, ContractComponent, CertificateComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SecretaryRouting
   ]
 })
 export class SecretaryModule { }
